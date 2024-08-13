1279c1b66685572b5dfc00b89fe06d731dfafad2
Jimdox <jimdo00001@gmail.com>
2024-08-13 14:44:26 +0700

    Update jimdohub.lua

--- jimdohub.lua
+++ jimdohub.lua
@@ -4,8 +4,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "jimdo Hub",
-    SubTitle = "Version 2",
+    Title = "jimdo Hub|Prenium",
+    SubTitle = "beta|join discord https://discord.com/invite/8BTpXhzqFh",
     TabWidth = 190,
     Size = UDim2.fromOffset(530, 350),
     Acrylic = false,
